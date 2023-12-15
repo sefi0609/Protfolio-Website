3c728c6aaf9c56ce214a08ba20ecc9c7be530008
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2023-12-15 18:16:16 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,7 +14,7 @@ with col1:
 with col2:
     st.title('Yosefi Kroytoro')
 
-    content = """I'm a computer science student at the open university, looking for a full time position.
+    content = """I have a BSc in computer science, cybersecurity program, from the open university.
         I'm working in a software company as a NOC operator, working with complex systems. 
         Acting as the first line of defence, collecting data, 
         responding to production issues and cyber attacks.
