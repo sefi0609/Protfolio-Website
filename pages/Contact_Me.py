920249a72440686122d1f744d9634218661d35ef
Yosefi Kroytoro <sefi0609@gmail.com>
2025-01-24 18:16:52 +0200

    change design

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -4,9 +4,10 @@ from send_email import send_email
 st.title('Contact Me')
 
 with st.form(clear_on_submit=True, key='form'):
-    email = st.text_input('Your email address')
-    raw_message = st.text_area('Your message')
+    email = st.text_input('Email address')
+    raw_message = st.text_area('Message')
     button = st.form_submit_button('Submit')
+
     # arrange the message
     message = f"""\
 Subject: from Portfolio website\n
@@ -14,9 +15,10 @@ From: {email}
 {raw_message}
 """
     if button:
+
         # if submit button was pressed check if those fields are not empty
         if email != '' and raw_message != '':
             send_email(message)
-            st.info('Your email was sent successfully')
+            st.info('Your message has been sent successfully. We will get in touch with you as soon as possible')
         else:
-            st.error('Please supply an email and message')
+            st.error('Please provide an email address and a message')
